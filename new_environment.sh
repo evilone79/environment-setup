07e80d3e657f7d37098e724600fae6d2fe1eb8d8
Mike D <evilone.coder@gmail.com>
2021-09-08 17:41:19 +0300

    added zsh stuff

--- new_environment.sh
+++ new_environment.sh
@@ -8,7 +8,7 @@ sudo apt install -y \
 	build-essential automake git cmake meld catfish git-cola cppcheck valgrind heaptrack heaptrack-gui \
 	vim htop synaptic flameshot tilix wireshark zeal geany geany-plugins \
 	exuberant-ctags cscope zip ccache curl wget default-jdk gettext python3-pip apt-transport-https \
-        ca-certificates gnupg lsb-release
+  ca-certificates gnupg lsb-release
 
 echo "Installing libraries"
 sudo apt install -y libncurses-dev python3-dev
@@ -24,8 +24,6 @@ cd ~/.vim/plugged/youcompleteme
 ./install.py --clangd-completer
 cd $workdir
 
-
-
 echo "Installing Docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
@@ -38,5 +36,14 @@ sudo apt -y install docker-ce docker-ce-cli containerd.io
 # Add user to docker group for using docker without sudo command.
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+sudo apt install zsh
+cp ./zshrc.tmpl ~/.zshrc
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ./zshrc.tmpl ~/.zshrc
+
+
 
 
