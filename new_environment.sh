9794e81c626ab5d35d29be243079177b3f75ba4c
evilone79 <evilone.coder@gmail.com>
2024-08-10 14:09:35 +0300

    added more stuff to ubuntu

--- new_environment.sh
+++ new_environment.sh
@@ -1,17 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt update
 sudo apt -y upgrade
 
 echo "Installing apps"
 sudo apt install -y \
-	build-essential g++ automake git cmake meld catfish git-cola cppcheck valgrind heaptrack heaptrack-gui \
-	vim htop synaptic flameshot tilix wireshark zeal geany geany-plugins \
-	exuberant-ctags cscope zip ccache curl wget default-jdk gettext python3-pip apt-transport-https \
-  ca-certificates gnupg lsb-release zsh baobab
+	build-essential g++ automake git cmake meld catfish cppcheck valgrind heaptrack heaptrack-gui vim htop synaptic terminator wireshark zeal exuberant-ctags cscope zip ccache curl wget default-jdk gettext \
+	python3-pip apt-transport-https ca-certificates gnupg lsb-release zsh baobab gawk wget git diffstat unzip \
+    texinfo chrpath socat xterm ncurses-dev lzop software-properties-common minicom adb cmake-format gdb-multiarch stlink-tools stlink-gui
 
-echo "Installing libraries"
-sudo apt install -y libncurses-dev python3-dev
+echo "Installing KVM stuff"
+sudo apt install -y \
+    qemu-kvm qemu-system qemu-utils libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager qemu-system-arm
+
+echo "Installing misc libraries"
+sudo apt install -y \
+    libncurses-dev python3-dev libelf-dev qtbase5-dev libqt5serialport5-dev
+
+echo "Installing Clang stuff"
+sudo apt install -y \
+    clang clang-tools clang-format clang-tidy
 
 echo "Installing VimPlug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -38,7 +46,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 cp ./zshrc.tmpl ~/.zshrc
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  "" --unattended
 cp ./zshrc.tmpl ~/.zshrc
